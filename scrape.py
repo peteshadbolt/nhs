ea8f3d5e3ebf9827ae364461f4bd12d749cbef00
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-05-02 22:35:36 +0200

    Ya that goes pretty fast now. Danger Will Robinson

--- scrape.py
+++ scrape.py
@@ -5,6 +5,11 @@ import sqlite3
 import csv
 import random
 import threading
+import Queue
+
+""" 
+Scrapes catchment areas from primarycare.nhs.uk. Is this evil?
+"""
 
 # Regexes
 CC0FINDER = re.compile(r"^\s*var\s*CC0\s*\=\s*\[new\sgoogle\.maps.*$", re.M)
@@ -27,7 +32,7 @@ def get_name(html):
     return matches.groups()[0] if matches else None
 
 def get_page(oc):
-    """ Get a page by the mystery key. Working example: ?oc=A85007&h=800&w=1000&if=0 """
+    """ Get a page by organization code. Working example: ?oc=A85007&h=800&w=1000&if=0 """
     url = "https://www.primarycare.nhs.uk/publicfn/catchment.aspx"
     payload = {"oc":oc, "h":1, "w":1, "if":0}
     r = requests.get(url, params = payload)
@@ -43,16 +48,42 @@ def get_ocs(filename="data/epraccur.csv"):
     f.close()
     return ocs
 
+class scraper(threading.Thread):
+    def __init__(self, ocs, queue):
+        """ A threaded scraper """
+        threading.Thread.__init__(self)
+        self.ocs = ocs
+        self.queue = queue
+
+    def run(self):
+        """ Scrape all ocs assigned to this thread """
+        print "Started thread {:}".format(threading.current_thread().name)
+        for index, oc in enumerate(self.ocs):
+            name, polygon = get_page(oc)
+            self.queue.put((oc, name, polygon))
+
+
 if __name__ == '__main__':
+    # Get a list of ids yet to scrape
     db_connection = sqlite.connect("practices.db")
     db_curs = db_connection.cursor()
-
     db_curs.execute("SELECT id FROM practices")
     done = set([x[0] for x in db_curs.fetchall()])
-    todo = get_ocs() - done
+    todo = list(get_ocs() - done)
+    main_queue = Queue.Queue()
 
-    for index, oc in enumerate(todo):
-        name, polygon = get_page(oc)
+    # Divide them into batches
+    MAX_THREADS = 10
+    BATCH_SIZE = len(todo)/MAX_THREADS
+    print "max threads: {:}\nbatch size: {:}".format(MAX_THREADS, BATCH_SIZE)
+    batches = [todo[i:i+BATCH_SIZE] for i in range(0, len(todo), BATCH_SIZE)]
+
+    scrapers = [scraper(batch, main_queue) for batch in batches]
+    for s in scrapers:
+        s.start()
+
+    while True:
+        oc, name, polygon = main_queue.get(True, 5)
         if not name: 
             db_curs.execute("INSERT INTO practices (id, broken)\
                              VALUES ('{0}', '{1}')".format(oc, 1))
